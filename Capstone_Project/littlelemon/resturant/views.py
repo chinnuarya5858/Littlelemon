eb6554749af0bbff2fea6f6df86939662e0e9952
Arya Kunjumon <aryach5858@gmail.com>
2025-01-08 09:58:35 +0530

    update it

--- Capstone_Project/littlelemon/resturant/views.py
+++ Capstone_Project/littlelemon/resturant/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from .models import Menu
+from .models import Booking
+from .serializer import BookingSerializer
+from .serializer import MenuSerializer
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import DestroyAPIView 
+from rest_framework import viewsets 
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html',{})
+class MenuView(ListCreateAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class SingleMenuView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
+
